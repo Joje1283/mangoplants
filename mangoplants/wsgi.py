09907dc4ae1f9449a9c8a5e65f538b91c11f1a14
Jaesik <joje1283@gmail.com>
2021-08-20 00:34:31 +0900

    Add static files. Add django template files.

--- mangoplants/wsgi.py
+++ mangoplants/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mangoplants project.
+WSGI config for commons project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mangoplants.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commons.settings')
 
 application = get_wsgi_application()
