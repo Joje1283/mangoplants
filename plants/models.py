764ea021a45a750aaa963a962997712d9477eeaa
Jaesik <joje1283@gmail.com>
2021-08-20 23:14:31 +0900

    Add plant model. Add code for media config.

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from commons.utils import uuid_name_upload_to
+
+
+class Plant(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to=uuid_name_upload_to)
+
+    def __str__(self):
+        return self.name
