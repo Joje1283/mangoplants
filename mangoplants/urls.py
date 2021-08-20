aa5e81598901ad48a14a06a29d25ea02d1a0a84a
Jaesik <joje1283@gmail.com>
2021-08-20 23:58:31 +0900

    Refactor template files.

--- mangoplants/urls.py
+++ mangoplants/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('commons.urls')),
+    path('posts/', include('posts.urls')),
 ]
 
 if settings.DEBUG:
