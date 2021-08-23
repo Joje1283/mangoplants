e1b341a97b0dd18550b2ba74681dccd0e9ed965a
Jaesik <joje1283@gmail.com>
2021-08-23 23:54:37 +0900

    Apply django template in index.html.

--- commons/views.py
+++ commons/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from plants.models import Plant
+from posts.models import Post
+
 
 def index(request):
-    return render(request, 'commons/index.html')
+    plant_qs = Plant.objects.all()
+    post_qs = Post.objects.all()
+    context = {
+        'plants': plant_qs,
+        'posts': post_qs
+    }
+    return render(request, 'commons/index.html', context=context)
