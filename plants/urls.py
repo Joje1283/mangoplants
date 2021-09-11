cdfc0c1b86b5950fdef5efda0d5d8730dfa0a9af
Jaesik <joje1283@gmail.com>
2021-09-11 20:05:53 +0900

    Add the page for plant create.

--- plants/urls.py
+++ plants/urls.py
@@ -5,8 +5,7 @@ app_name = 'plants'
 
 urlpatterns = [
     path('', views.plant_list, name='plant_list'),
-    # path('<int:pk>/', views.plant_detail, name='plant_detail'),
-    # path('new/', views.plant_create, name='plant_create'),
+    path('new/', views.plant_create, name='plant_create'),
     # path('<int:pk>/edit', views.plant_update, name='plant_update'),
     # path('<int:pk>/delete', views.plant_delete, name='plant_delete'),
 ]
--- plants/views.py
+++ plants/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Plant
+from .forms import PlantForm
 
 
-def plant_list(requeset):
+def plant_list(request):
     plants = Plant.objects.all()
-    return render(requeset, 'plants/plant_list.html', {'plants': plants})
+    return render(request, 'plants/plant_list.html', {'plants': plants})
+
+
+def plant_create(request):
+    if request.method == 'POST':
+        plant_form = PlantForm(request.POST, request.FILES)
+        if plant_form.is_valid():
+            plant_form.save()
+            return redirect('plants:plant_list')
+    else:
+        plant_form = PlantForm()
+    return render(request, 'plants/plant_form.html', {'form': plant_form})
