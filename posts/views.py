694d5c04a673c97ecb5f407d30ab4ac124000044
Jaesik <joje1283@gmail.com>
2021-09-11 17:56:11 +0900

    Add the create page for posts.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def posts(request):
@@ -7,6 +8,17 @@ def posts(request):
     return render(request, 'posts/post_list.html', context={'post_qs': post_qs})
 
 
-def post(request, pk):
+def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'posts/post_detail.html', context={'post': post})
+
+
+def post_create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('posts:post_detaildetail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': post_form})
